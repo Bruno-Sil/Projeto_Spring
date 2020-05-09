3a3c639f5de46f4943562ceaec51524c0ba4eef4
Bruno Sil <deltafid@gmail.com>
2020-05-09 19:30:14 -0300

    comitando_arquivos

--- src/main/java/br/pro/bruno/drogaria/controller/CategoriaController.java
+++ src/main/java/br/pro/bruno/drogaria/controller/CategoriaController.java
@@ -1,9 +1,12 @@
 package br.pro.bruno.drogaria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +21,7 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	 
-	@GetMapping
+	 @GetMapping
      public List<Categoria> listar() {
     	List<Categoria> categorias = categoriaRepository.findAll();
     	 return categorias;
@@ -28,5 +31,12 @@ public class CategoriaController {
 	public Categoria inserir (@RequestBody Categoria categoria) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		 return categoriaSalva;
+	}
+	
+	@DeleteMapping("/{codigo}")
+	public Categoria excluir (@PathVariable Short codigo) {
+		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		categoriaRepository.delete(categoria.get());
+		return categoria.get();
 	}
 }
