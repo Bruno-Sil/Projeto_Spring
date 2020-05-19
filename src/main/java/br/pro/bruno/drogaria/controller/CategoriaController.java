9cb93c4669fcabb84c944b3e692b4df3a848d0c2
Bruno Sil <deltafid@gmail.com>
2020-05-19 14:09:50 -0300

    Projeto_Spring

--- src/main/java/br/pro/bruno/drogaria/controller/CategoriaController.java
+++ src/main/java/br/pro/bruno/drogaria/controller/CategoriaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,13 @@ public class CategoriaController {
     	List<Categoria> categorias = categoriaRepository.findAll();
     	 return categorias;
      }
+	 
+	 @GetMapping("/{codigo}")
+	 public Categoria buscar(Short codigo) {
+		 Optional<Categoria> resultado = categoriaRepository.findById(codigo);
+		 Categoria categoria = resultado.get();
+		 return categoria;
+	 }
 	
 	@PostMapping
 	public Categoria inserir (@RequestBody Categoria categoria) {
@@ -39,5 +47,11 @@ public class CategoriaController {
 		categoriaRepository.delete(categoria.get());
 		Categoria categoriaRetorno = categoria.get();
 		return categoriaRetorno;
+	}
+	
+	@PutMapping
+	public Categoria editar(@RequestBody Categoria categoria ) {
+		Categoria categoriaEditada = categoriaRepository.save(categoria);
+		return categoriaEditada;
 	}
 }
